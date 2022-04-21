46f4cfdd0da5d7ac3f466bfca365390692888c89
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2022-04-21 15:36:32 -0300

    Adjust colum type database mapper

--- src/modules/users/entities/User.ts
+++ src/modules/users/entities/User.ts
@@ -3,15 +3,16 @@ import {
   CreateDateColumn,
   Entity,
   OneToMany,
-  PrimaryGeneratedColumn
-} from 'typeorm';
-import { v4 as uuid } from 'uuid';
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
 
-import { Statement } from '../../statements/entities/Statement';
+import { Statement } from "../../statements/entities/Statement";
 
-@Entity('users')
+@Entity("users")
 export class User {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id?: string;
 
   @Column()
@@ -23,17 +24,17 @@ export class User {
   @Column()
   password: string;
 
-  @OneToMany(() => Statement, statement => statement.user)
+  @OneToMany(() => Statement, (statement) => statement.user)
   statement: Statement[];
 
   @CreateDateColumn()
   created_at: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
-    if(!this.id) {
+    if (!this.id) {
       this.id = uuid();
     }
   }
